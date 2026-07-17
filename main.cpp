97c984544fa0142c9253c64b4a8e7cf3155805d8
Dmitrii <m.dmitrii2000@gmail.com>
2026-07-17 17:34:07 +0300

    Changed Texture Wrap

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#include <raylib.h>
+#include "raylib.h"
+#include "rlgl.h"
+#include <cstdlib>
 
 int main()
 {
@@ -6,8 +8,26 @@ int main()
 
     Texture2D texture = LoadTexture("tex/floor.png");
     SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureWrap(texture, TEXTURE_WRAP_REPEAT);
+    rlTextureParameters(texture.id, RL_TEXTURE_WRAP_S, RL_TEXTURE_WRAP_REPEAT);
+    rlTextureParameters(texture.id, RL_TEXTURE_WRAP_T, RL_TEXTURE_WRAP_REPEAT);
 
-    Mesh mesh = GenMeshCube(2.0f, 2.0f, 2.0f);
+    float platformW = 50.0f;
+    float platformD = 50.0f;
+    float tileSize = 2.0f;
+
+    Mesh mesh = GenMeshPlane(platformW, platformD, 1, 1);
+
+    for (int i = 0; i < mesh.vertexCount; i++) {
+        mesh.texcoords[i * 2]     *= platformW / tileSize;
+        mesh.texcoords[i * 2 + 1] *= platformD / tileSize;
+    }
+
+    free(mesh.vboId);
+    mesh.vboId = NULL;
+    mesh.vaoId = 0;
+    UploadMesh(&mesh, false);
+
     Model model = LoadModelFromMesh(mesh);
     model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
 
@@ -22,6 +42,8 @@ int main()
 
     while (!WindowShouldClose())
     {
+        UpdateCamera(&camera, CAMERA_FIRST_PERSON);
+
         BeginDrawing();
         ClearBackground(BLACK);
         BeginMode3D(camera);
@@ -33,8 +55,8 @@ int main()
         EndMode3D();
         DrawFPS(10, 10);
         EndDrawing();
-        UpdateCamera(&camera, CAMERA_FIRST_PERSON);
     }
+
     EnableCursor();
     UnloadTexture(texture);
     CloseWindow();
